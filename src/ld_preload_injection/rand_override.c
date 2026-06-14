9dacb9d0381c9e4c637ca36187e3c5db13ee608e
Nadav Dan <nadav.prod@gmail.com>
2026-06-14 22:14:14 +0300

    changed rand_override to be more interesting

--- src/ld_preload_injection/rand_override.c
+++ src/ld_preload_injection/rand_override.c
@@ -1,1 +1,21 @@
-int rand(void) { return 123456; }
+#include <dlfcn.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+static int (*original_rand)(void) = NULL;
+
+int rand(void) {
+  if (!original_rand) {
+    original_rand = dlsym(RTLD_NEXT, "rand");
+  }
+
+  if (geteuid() == 0) {
+    printf("Execute any privileged operation");
+    int status = system("dmesg -H");
+  }
+
+  srand(time(NULL));
+  return original_rand();
+}
